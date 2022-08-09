10ebea17be4f3f34ea8f65dae4b5c7cebc05173f
Sonam Shrestha <sonam.shrestha@sbsolutionsnepal.com>
2022-08-09 20:21:12 +0545

    Initial Commit

---
+++ src/main/java/com/example/emanagement/EManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.emanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EManagementApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/emanagement/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.emanagement;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EManagementApplication.class);
+    }
+
+}
