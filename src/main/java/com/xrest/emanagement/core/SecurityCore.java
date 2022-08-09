9c4c975ffa5058b1ca650f229da9bdbf6bd43091
Sonam Shrestha <sonam.shrestha@sbsolutionsnepal.com>
2022-08-09 21:13:54 +0545

    Change Ser

---
+++ src/main/java/com/xrest/emanagement/core/SecurityCore.java
@@ -0,0 +1,26 @@
+package com.xrest.emanagement.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityCore extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswrodEncode() {
+        return  new BCryptPasswordEncoder();
+    }
+}
