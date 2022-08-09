0363877b7fb10b3bf6fa7eed3363be7295337bb2
Sonam Shrestha <sonam.shrestha@sbsolutionsnepal.com>
2022-08-09 22:32:29 +0545

    JWT authorization

--- src/main/java/com/xrest/emanagement/core/JWT/Authorization.java
+++ src/main/java/com/xrest/emanagement/core/JWT/Authorization.java
@@ -1,8 +1,12 @@
 package com.xrest.emanagement.core.JWT;
 
 import antlr.TokenStreamException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.xrest.emanagement.core.JWTCONSTANT;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -19,10 +23,15 @@ public class Authorization extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
-            String token  = request.getHeader(JWTCONSTANT.TYPE);
-            if (null == token || !token.contains(JWTCONSTANT.PREFIX)) {
+            String bearer  = request.getHeader(JWTCONSTANT.TYPE);
+            if (null == bearer || !bearer.contains(JWTCONSTANT.PREFIX)) {
                 chain.doFilter(request,response);
             }
+            String token = bearer.substring(6);
+            String username = JWT.require(Algorithm.HMAC512(JWTCONSTANT.KEY.getBytes())).build().verify(token).getSubject();
+            //user repository
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username,null,null);
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
         } catch (Exception e) {
 
         }
